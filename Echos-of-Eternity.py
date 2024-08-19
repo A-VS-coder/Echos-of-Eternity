95b6b719530f2ceab81a47707789b0e64527e4b5
AVS <adityasurve251206@gmail.com>
2024-08-19 11:14:18 +0300

    initial commit
    added the imports that will be required
    initialized the player dictionaries

--- Echos-of-Eternity.py
+++ Echos-of-Eternity.py
@@ -1,1 +1,52 @@
 #Echos of Eternity
+
+#imports
+import time
+import os
+import random
+
+#constants
+player_1 = {
+    "state": None,
+    "level": 1,
+    "xp":0,
+    "money": 0,
+    #player stats
+    "strength": 0, "speed": 0,"health max":0, "health":0, "stamina max":0, "stamina":0, "mana max":0, "mana":0,
+    #inventory: {wpn name:[sprite/img/symbol], qty, dmg, price}
+    "inventory": {
+        "kratos": ['✠', 0, 0, 0], 
+        "knights Sword": ['🗡', 0, 0, 0], 
+        "biezelbub": ['⬤', 0, 0, 0], 
+        "zues": ['ϟ', 0, 0, 0], 
+        "andromeda":['❃', 0, 0, 0]
+    },
+    "equip": {
+        "weapon": "",
+        "moves": []
+    }
+}
+
+player_2 = {
+    "state": None,
+    "level": 1,
+    "xp":0,
+    "money": 0,
+    #player stats
+    "strength": 0, "speed": 0,"health max":0, "health":0, "stamina max":0, "stamina":0, "mana max":0, "mana":0,
+    #inventory: {wpn name:[sprite/img/symbol], qty, dmg, price}, max size is 50
+    "inventory": {
+        "kratos": ['✠', 0, 0, 'God Slayer', 'Spinning Axe', 0], 
+        "knights Sword": ['🗡', 0, 0, 'Dark Blade', 0], 
+        "biezelbub": ['⬤', 0, 0, 0], 
+        "zues": ['ϟ', 0, 0, 0], 
+        "andromeda":['❃', 0, 0, 0]
+    },
+    "equip": {
+        "weapon": "",
+        "moves": []
+    }
+}
+
+active = player_1
+other = player_2
